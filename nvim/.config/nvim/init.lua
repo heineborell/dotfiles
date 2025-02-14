5c89c0a85ac455778190ed00ee19ec0f0fb43853
Deniz Deveci <dodeve@gmail.com>
2025-02-15 05:47:42 +0900

    added obsidian

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -867,18 +867,30 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     --'folke/tokyonight.nvim',
-    'nordtheme/vim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'nord'
+
+    'rjshkhr/shadow.nvim',
+    priority = 1000,
+    config = function()
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme 'shadow'
       vim.cmd 'highlight Normal guibg=#1B1212'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+
+    -- 'nordtheme/vim',
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- init = function()
+    --   -- Load the colorscheme here.
+    --   -- Like many other themes, this one has different styles, and you could load
+    --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --   vim.cmd.colorscheme 'nord'
+    --   vim.cmd 'highlight Normal guibg=#1B1212'
+    --
+    --   -- You can configure highlights by doing something like:
+    --   vim.cmd.hi 'Comment gui=none'
+    -- end,
   },
 
   -- Highlight todo, notes, etc in comments
@@ -974,6 +986,7 @@ require('lazy').setup({
   require 'kickstart.plugins.jupynium',
   require 'kickstart.plugins.flash',
   require 'kickstart.plugins.tmuxmove',
+  require 'kickstart.plugins.obsidian',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   --
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
