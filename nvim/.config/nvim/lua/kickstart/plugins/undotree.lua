b6252c45cf6b254485218c87130fd07c143e8ad1
Deniz Deveci <dodeve@gmail.com>
2025-07-07 18:14:33 +0900

    added undo tree

--- nvim/.config/nvim/lua/kickstart/plugins/undotree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/undotree.lua
@@ -3,4 +3,12 @@ return {
   keys = {
     { '<leader>ut', vim.cmd.UndotreeToggle, desc = 'Toggle Undotree' },
   },
+  init = function()
+    local undodir = vim.fn.stdpath 'cache' .. '/undo'
+    if vim.fn.isdirectory(undodir) == 0 then
+      vim.fn.mkdir(undodir, 'p')
+    end
+    vim.o.undodir = undodir
+    vim.o.undofile = true
+  end,
 }
