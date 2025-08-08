239b667b4a9304392dbbfd5a11251b7320ba4344
Deniz Deveci <dodeve@gmail.com>
2025-08-09 02:42:51 +0900

    added oil nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1056,7 +1056,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.database',
   require 'kickstart.plugins.markdownprev',
   require 'kickstart.plugins.snacks',
@@ -1071,6 +1071,7 @@ require('lazy').setup({
   require 'kickstart.plugins.yazi',
   require 'kickstart.plugins.undotree',
   require 'kickstart.plugins.translate',
+  require 'kickstart.plugins.oil',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
